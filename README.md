2389e5770601cb5b94d36a3c0e5e9a2589af8a93
sliwekok <dominiksliwek@gmail.com>
2023-11-21 20:22:01 +0100

    configuration init, setting connection to db, creating webpack, auth components

---
+++ README.md
@@ -0,0 +1,4 @@
+# Description
+
+Webapp that creates your free minecraft server! Offers web-menagment, adding forge, optifine, custom mods and many more!
+
---
+++ assets/app.js
@@ -0,0 +1,15 @@
+// import scss
+import './styles/app.scss';
+
+// create global $ and jQuery variables
+const $ = require('jquery');
+global.$ = global.jQuery = $;
+
+require('./tooltips');
+require('./main.js');
+require('./watched.js');
+require('./bootstrap.js');
+require('./account.js');
+require('./addNew');
+require('./itemFetcher');
+require('./search');
