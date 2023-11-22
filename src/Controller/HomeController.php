94f700396c9ad8720ba591ec3a7d27acc80d97f9
sliwekok <dominiksliwek@gmail.com>
2023-11-22 03:01:33 +0100

    changed colours palette, added landing page description, styling

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('landing.html.twig', []);
+    }
+
+    #[Route('/pricing', name: 'pricing')]
+    public function pricing(): Response
+    {
+        return $this->render('pricing.html.twig', []);
+    }
+}
