d8cd562a9afc3c8acaf66a85d1a373b9dd0f6699
sliwekok <dominiksliwek@gmail.com>
2024-09-26 09:17:55 +0200

    deleted invalid quote

--- src/UniqueNameInterface/ServerUnixCommandsInterface.php
+++ src/UniqueNameInterface/ServerUnixCommandsInterface.php
@@ -18,7 +18,7 @@
         public const SCREEN_GETSPECIFICPID = "sudo screen -ls | grep -w '". self::REPLACEMENT_NAME ."' | awk '{print $1}' | cut -d. -f1";
         public const SCREEN_GETCURRENTPID = 'echo $STY';
 
-        public const RUN_SERVER = 'sudo java -Xmx'. self::REPLACEMENT_RAM. 'G -jar '. self::REPLACEMENT_PATH. '/server.jar --nogui"';
+        public const RUN_SERVER = 'sudo java -Xmx'. self::REPLACEMENT_RAM. 'G -jar '. self::REPLACEMENT_PATH. '/server.jar --nogui';
 
         public const KILL_SERVER = 'sudo screen -S '. self::REPLACEMENT_NAME. ' -X quit';
 
