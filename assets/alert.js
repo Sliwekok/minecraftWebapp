32f816804243095b3e486ab904f6cc4ff2c7a75c
sliwekok <dominiksliwek@gmail.com>
2024-10-20 00:20:14 +0200

    added html support on alerts

--- assets/alert.js
+++ assets/alert.js
@@ -22,7 +22,7 @@ export function showAlert(status, message, header, alertConfirmation, isTemporar
 
     alert.addClass('alert-'+className).fadeIn(100);
     alertHeader.text(header);
-    alertContent.text(message);
+    alertContent.html(message);
 
     if (isTemporary) {
         setTimeout(function() {
